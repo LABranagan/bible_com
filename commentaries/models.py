95583acd9c47e0b3434014ff722618b2725566e6
Lyle A Branagan <lyle.branagan@gmail.com>
2019-08-28 22:15:48 -0400

    plan for models

--- commentaries/models.py
+++ commentaries/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# Model:  title, reference, slug, verse, concept, comments, created,
+# #    updated, status, key words
+## Key words:  word, definition
+
+#TODO Add models for each commentary
+
+#TODO Load and save lists of commentaries
