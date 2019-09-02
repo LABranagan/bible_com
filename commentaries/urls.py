5442a0ce2f9fc4e9950f4d673dfe1d9865b7836c
Lyle A Branagan <lyle.branagan@gmail.com>
2019-09-02 12:26:12 -0400

    Start CRUD with crispy_forms with minor edits

--- commentaries/urls.py
+++ commentaries/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import PostCreateView, PostListView, list_posts, update_post
 
 urlpatterns = [
-    # path('list/', PostListView.as_view(), name='list_post'),
+    # path('list/', PostListView.as_view(), name='list_posts'),
     path('list/', list_posts, name='list_posts'),
     path('update/<slug:slug>', update_post, name='update_post'),
     path('add/', PostCreateView.as_view(), name='create_post'),
